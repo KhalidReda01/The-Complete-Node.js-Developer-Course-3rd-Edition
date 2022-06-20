a0e7848680213a8edf2e09df6355058524cbec6b
khalidreda <khalidaziz2198@gmail.com>
2022-06-20 07:35:13 +0200

    challenge solved

--- 3.Node.js Module System/node-course/notes-app/app.js
+++ 3.Node.js Module System/node-course/notes-app/app.js
@@ -2,7 +2,9 @@ const fs=require('fs')
 
 // fs.writeFileSync('notes.txt', 'My name is Khalid');
 
-// Challenge Time
+// Challenge Time 
+// Append a message to notes.txt any message Khalid actually It's the same thing 
 // 1.use appendFileSync to append to the file
 // 2.run the script
 // 3.check your work by opening the file and viewing the appended text 
+fs.writeFileSync('my notes.txt', "Okay we will see this new message at the new file")
