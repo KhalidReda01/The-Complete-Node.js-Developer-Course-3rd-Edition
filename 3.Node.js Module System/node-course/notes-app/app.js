771e36950213ad1988400ee5e6d7bd67a28dc8c6
khalidreda <khalidaziz2198@gmail.com>
2022-06-20 07:33:19 +0200

    First Challenge Time at the class easy one let's do it

--- 3.Node.js Module System/node-course/notes-app/app.js
+++ 3.Node.js Module System/node-course/notes-app/app.js
@@ -1,3 +1,8 @@
 const fs=require('fs')
 
-fs.writeFileSync('notes.txt', 'This file was created by node.js!')
+// fs.writeFileSync('notes.txt', 'My name is Khalid');
+
+// Challenge Time
+// 1.use appendFileSync to append to the file
+// 2.run the script
+// 3.check your work by opening the file and viewing the appended text 
