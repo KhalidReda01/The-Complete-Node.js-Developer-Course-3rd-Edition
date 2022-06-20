5b0c16fdd373f78e44aad36603f83c04abd3db90
khalidreda <khalidaziz2198@gmail.com>
2022-06-20 09:00:31 +0200

    Printing in Color

--- 3.Node.js Module System/node-course/notes-app/app.js
+++ 3.Node.js Module System/node-course/notes-app/app.js
@@ -1,5 +1,14 @@
-const validator=require('validator')
+// const validator=require('validator')
 const getNotes = require('./notes.js')
 console.log(getNotes())
 // console.log(validator.isEmail('khalid@gmail.com'))
-console.log(validator.isURL('https://mead.io'))
+// console.log(validator.isURL('https://mead.io'))
+
+// Challenge Time 
+/**
+ * Use The Chalk library in your Project 
+ * 1.install version 2.4.1 of chalk 
+ * 2. Load chalk into app.js
+ * 3. use it to print the string "Sucess!" to the console in green 
+ * 4. Test your work
+ */
