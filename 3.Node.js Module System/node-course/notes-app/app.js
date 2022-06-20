f058c8fe15e1b69a4849cfb4b179d6f4db587179
khalidreda <khalidaziz2198@gmail.com>
2022-06-20 08:02:14 +0200

    8m

--- 3.Node.js Module System/node-course/notes-app/app.js
+++ 3.Node.js Module System/node-course/notes-app/app.js
@@ -1,12 +1,4 @@
-const fs=require('fs')
+const name=require('./utils.js')
 
-fs.writeFileSync('notes.txt', 'My name is Khalid.');
 
-fs.appendFileSync('notes.txt', ' I live in Mansura.')
-
-// Challenge Time 
-// Append a message to notes.txt any message Khalid actually It's the same thing 
-// 1.use appendFileSync to append to the file
-// 2.run the script
-// 3.check your work by opening the file and viewing the appended text 
-// fs.writeFileSync('my notes.txt', "I'm khalid I live in Mansura. ")
+console.log(name)
