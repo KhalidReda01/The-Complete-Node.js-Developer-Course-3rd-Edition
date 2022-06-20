2d87c8cb036bcea4b136a3ad10d8a968faa5397f
khalidreda <khalidaziz2198@gmail.com>
2022-06-20 08:43:56 +0200

    Lecture 4 Importing npm Modules CAP

--- 3.Node.js Module System/node-course/notes-app/app.js
+++ 3.Node.js Module System/node-course/notes-app/app.js
@@ -1,2 +1,5 @@
-const getNotes=require('./notes.js')
+const validator=require('validator')
+const getNotes = require('./notes.js')
 console.log(getNotes())
+// console.log(validator.isEmail('khalid@gmail.com'))
+console.log(validator.isURL('https://mead.io'))
