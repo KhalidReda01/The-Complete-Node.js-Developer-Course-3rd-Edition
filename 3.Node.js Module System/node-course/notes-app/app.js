622a400f355135d0cd48e93f750615cc0f0cfdcd
khalidreda <khalidaziz2198@gmail.com>
2022-06-20 08:18:38 +0200

    Lecture 3 CAP Importing Your Own files

--- 3.Node.js Module System/node-course/notes-app/app.js
+++ 3.Node.js Module System/node-course/notes-app/app.js
@@ -1,14 +1,2 @@
 const getNotes=require('./notes.js')
 console.log(getNotes())
-
-/**
- * Challenge time
- * 1.create a new file called notes.js
- * 2.create getNotes function that returns "My notes...."
- * 3.Export getNotes function 
- * 4.From app.js , load in an call the function message to console
- */
-
-// const add=require('./utils.js')
-// const sum =add(4,-2)
-// console.log(sum)
