56dd882aeff74cdbc0cf103f17eb1ac07b535b60
khalidreda <khalidaziz2198@gmail.com>
2022-06-21 09:42:08 +0200

    Challenge Time lecture 3 Argument Parsing with Yargs part !

--- 4.File System and Command Line Args/node-course/notes-app/app.js
+++ 4.File System and Command Line Args/node-course/notes-app/app.js
@@ -1,11 +1,35 @@
 
 const getNotes = require('./notes.js')
 const chalk = require('chalk');
+const yargs = require('yargs');
 
-const command = process.argv[2]
-console.log(process.argv)
-if (command==='add') {
-  console.log('Adding note!')
-} else if (command === 'remove') {
-  console.log('Removing note!')
-}
+// Customize yargs version
+yargs.version('1.1.0')
+
+// create add command
+yargs.command({
+  command: 'add',
+  describe: 'Add a new note',
+  handler: function () {
+    console.log('Adding a new note!')
+  }
+})
+// create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function () {
+    console.log('Removing the note')
+  }
+})
+// add,remove,read,list
+/**
+ * Challenge Time
+ * Challenge : Add two new commands
+ * 
+ * 1. setup command to support "list" command (Print placeholder messsage for now)
+ * 2. setup command to support "read" command (Print placeholder message for now)
+ * 3. Test your work by runing both commands and ensure correct output
+ */
+console.log(yargs.argv)
+
