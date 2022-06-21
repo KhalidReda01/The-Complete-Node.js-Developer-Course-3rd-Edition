fd3b8d593c203ada0e7a54bafcd2ce8c831d34d5
khalidreda <khalidaziz2198@gmail.com>
2022-06-21 09:14:15 +0200

    Lecture 2 Getting inputs from users 5m

--- 4.File System and Command Line Args/node-course/notes-app/app.js
+++ 4.File System and Command Line Args/node-course/notes-app/app.js
@@ -3,4 +3,5 @@ const getNotes = require('./notes.js')
 console.log(getNotes())
 
 const chalk = require('chalk');
-console.log(chalk.green.inverse.bold('Sucess!'))
+console.log(chalk.blue.inverse.bold('Sucess!'))
+console.log(process.argv[2])
