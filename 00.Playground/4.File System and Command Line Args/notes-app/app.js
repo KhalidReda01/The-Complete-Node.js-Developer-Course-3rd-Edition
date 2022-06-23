7908b8b52196fdbc83ab86ad988cb18171a41398
khalidreda <khalidaziz2198@gmail.com>
2022-06-23 15:16:27 +0200

    Lecture 15/2 Getting input from users done CFSC

--- 00.Playground/4.File System and Command Line Args/notes-app/app.js
+++ 00.Playground/4.File System and Command Line Args/notes-app/app.js
@@ -1,8 +1,10 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
 
-const msg = getNotes()
-console.log(msg)
 
-const greenMsg = chalk.blue.inverse.bold('Success!')
-console.log(greenMsg)
+const command = process.argv[2]
+if (command === 'add') {
+    console.log('Adding note!')
+} else if (command === 'remove') {
+    console.log('Removing note!')
+}
