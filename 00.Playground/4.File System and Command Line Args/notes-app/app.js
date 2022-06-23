11bea0ac6b3986c4c1dec89f136f75c7a5a5210d
khalidreda <khalidaziz2198@gmail.com>
2022-06-23 17:20:57 +0200

    DEES done for the handler letter lecture 16/3 cFSC done

--- 00.Playground/4.File System and Command Line Args/notes-app/app.js
+++ 00.Playground/4.File System and Command Line Args/notes-app/app.js
@@ -1,10 +1,39 @@
 const chalk = require('chalk')
 const getNotes = require('./notes.js')
+const yargs = require('yargs')
 
-
-const command = process.argv[2]
-if (command === 'add') {
-    console.log('Adding note!')
-} else if (command === 'remove') {
-    console.log('Removing note!')
-}
+// Customize yargs version
+yargs.version('1.1.0')
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+// create remove command 
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+})
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'List your notes',
+    handler: function () {
+        console.log('Listing out all notes')
+    }
+})
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function () {
+        console.log('Reading a note')
+    }
+})
+console.log(yargs.argv)
