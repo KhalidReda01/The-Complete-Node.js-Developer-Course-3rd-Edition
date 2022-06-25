1be3431aed7224bfe4a41705dc2e37aa4c70f869
khalidreda <khalidaziz2198@gmail.com>
2022-06-25 06:09:12 +0200

    DEES the same  one internal/modules/cjs/loader.js:905   throw err;   ^
    Error: Cannot find module

--- 00.Playground/4.File System and Command Line Args/playground/1-.json.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const user = JSON.parse(dataJSON)
-user.name = "Khalid Reda",
-  user.age = "23"
-const userJSON = JSON.stringify(usewr)
-fs.writeFileSync('1-json.json',userJSON)
