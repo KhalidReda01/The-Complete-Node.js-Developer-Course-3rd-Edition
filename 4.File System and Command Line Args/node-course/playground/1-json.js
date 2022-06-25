0d4cc9d186eac125f7290f566d7a1a9bf7ec4db5
khalidreda <khalidaziz2198@gmail.com>
2022-06-25 04:28:04 +0200

    11m

--- 4.File System and Command Line Args/node-course/playground/1-json.js
+++ 4.File System and Command Line Args/node-course/playground/1-json.js
@@ -1,11 +1,12 @@
-const book = {
-  title: 'Ego is the Enemy',
-  author:'Ryan Holiday',
-}
+const fs = require('fs')
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author:'Ryan Holiday',
+// }
 
-const bookJSON = JSON.stringify(book)
-console.log(book)
-console.log(bookJSON)
-
-const parseData = JSON.parse(bookJSON)
-console.log(parseData)
+// const bookJSON = JSON.stringify(book)
+// fs.writeFileSync('1-json.json',bookJSON)
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataJSON = dataBuffer.toString()
+const data =JSON.parse(dataJSON)
+console.log(dataBuffer.toString())
