5312d175a72ce81b42f18e2ddad877a61d37bb11
khalidreda <khalidaziz2198@gmail.com>
2022-06-25 04:37:55 +0200

    Challenge Time

--- 4.File System and Command Line Args/node-course/playground/1-json.js
+++ 4.File System and Command Line Args/node-course/playground/1-json.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+
 // const book = {
 //   title: 'Ego is the Enemy',
 //   author:'Ryan Holiday',
@@ -6,7 +7,16 @@ const fs = require('fs')
 
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json',bookJSON)
-const dataBuffer = fs.readFileSync('1-json.json')
-const dataJSON = dataBuffer.toString()
-const data =JSON.parse(dataJSON)
-console.log(dataBuffer.toString())
+// const dataBuffer = fs.readFileSync('1-json.json')
+// const dataJSON = dataBuffer.toString()
+// const data =JSON.parse(dataJSON)
+// console.log(data.title)
+
+
+/**
+ * Challenge Time
+ * 1.Load and parse the JSON data
+ * 2.Change the name and age property using the info
+ * 3.Stringify the changed object and overwrite the original data
+ * 4.Test your work by viewing data in the JSON file
+ */
