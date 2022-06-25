19ccc80b87a32453749bea6b90eb178938d9876c
khalidreda <khalidaziz2198@gmail.com>
2022-06-25 04:44:43 +0200

    Challenge solved

--- 4.File System and Command Line Args/node-course/playground/1-json.js
+++ 4.File System and Command Line Args/node-course/playground/1-json.js
@@ -20,3 +20,17 @@ const fs = require('fs')
  * 3.Stringify the changed object and overwrite the original data
  * 4.Test your work by viewing data in the JSON file
  */
+
+
+//1.Load and parse the JSON data
+const dataBuffer = fs.readFileSync('1-json.json')
+const dataaJSON = dataBuffer.toString()
+const data = JSON.parse(dataaJSON)
+
+console.log(data)
+//2.Change the name and age property using the info
+console.log(data.name = "yousef")
+console.log(data)
+//3.Stringify the changed object and overwrite the original data
+const dataJSON = JSON.stringify(data)
+fs.writeFileSync('1-json.json',dataJSON)
