3334c2d3f3619e66edc5d94b7706e63f5f43c13f
khalidreda <khalidaziz2198@gmail.com>
2022-06-27 06:46:19 +0200

    old way for solving this binding problem

--- 4.File System and Command Line Args/node-course/playground/2-arrow-function.js
+++ 4.File System and Command Line Args/node-course/playground/2-arrow-function.js
@@ -7,3 +7,16 @@
 // }
 // const square = (x) => x * x;
 // console.log(square(2))
+
+const event = {
+  name: 'Birthday Party',
+  guestList:['Khalid','Reda','Youssef'],
+  printGuestList() {
+    const that=this
+    console.log('Guest list for ' + this.name)
+    this.guestList.forEach(function (guest) {
+      console.log(guest+' is attending '+that.name)
+    })
+  }
+}
+event.printGuestList()
