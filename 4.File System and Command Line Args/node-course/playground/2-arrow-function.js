8413049fe3ad2502b7d42018b02a8312d736aa08
khalidreda <khalidaziz2198@gmail.com>
2022-06-27 06:47:14 +0200

    Lecture 21 Es6 Aside arrow function Done

--- 4.File System and Command Line Args/node-course/playground/2-arrow-function.js
+++ 4.File System and Command Line Args/node-course/playground/2-arrow-function.js
@@ -12,10 +12,9 @@ const event = {
   name: 'Birthday Party',
   guestList:['Khalid','Reda','Youssef'],
   printGuestList() {
-    const that=this
     console.log('Guest list for ' + this.name)
-    this.guestList.forEach(function (guest) {
-      console.log(guest+' is attending '+that.name)
+    this.guestList.forEach( (guest)=> {
+      console.log(guest+' is attending '+this.name)
     })
   }
 }
