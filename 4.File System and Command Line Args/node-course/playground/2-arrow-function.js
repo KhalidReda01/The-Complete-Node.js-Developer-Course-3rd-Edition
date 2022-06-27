5ccb0d9c71272a10ff88bf53808a6630e67a1453
khalidreda <khalidaziz2198@gmail.com>
2022-06-27 06:40:57 +0200

    1

--- 4.File System and Command Line Args/node-course/playground/2-arrow-function.js
+++ 4.File System and Command Line Args/node-course/playground/2-arrow-function.js
@@ -1,4 +1,9 @@
-const square = function (x) {
-  return x*x
-}
-console.log(square(3))
+// const square = function (x) {
+//   return x*x
+// }
+
+// const square = (x) => {
+//   return x*x
+// }
+// const square = (x) => x * x;
+// console.log(square(2))
